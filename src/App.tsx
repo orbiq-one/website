44124ece637024e7c18f010bbe0f4e79fbfc2d5d
Leonhard Breuer <git@leonhardbreuer.de>
2025-11-12 18:54:25 +0100

    feat: update website

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,28 @@ import type { Component } from 'solid-js';
 import OrbiqDark from "./assets/orbiq_dark.svg"
 import styles from './App.module.css';
 import ComingSoonComponent from './components/comingsoon';
+
 const App: Component = () => {
   return (
     <div class={styles.App}>
-      <img src={OrbiqDark}/>
+      <div
+        class={styles.bg}
+        style={{
+          background: `
+            radial-gradient(circle at 50% 50%,
+              rgba(226, 232, 240, 0.2) 0%,
+              rgba(226, 232, 240, 0.1) 25%,
+              rgba(226, 232, 240, 0.05) 35%,
+              transparent 50%
+            )
+          `,
+        }}
+      />
+      <img src={OrbiqDark} alt="Orbiq" />
       <h1 class="m-plus-code-latin-400" style="font-size: 4em;">Orbiq</h1>
-      <ComingSoonComponent />
     </div>
   );
 };
+
 
 export default App;
